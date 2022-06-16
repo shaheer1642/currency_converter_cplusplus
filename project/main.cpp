ca3d81c50847a51f1efd8eee28740179322ee9ed
MrSofty123 <shaheer1642@gmail.com>
2022-06-16 16:49:57 +0500

    json read

--- project/main.cpp
+++ project/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include "lib\json.hpp"
+
 using namespace std;
+using json = nlohmann::json;
+
+
+class Currency {
+	private:
+    	json currencyData;
+
+    public: 
+	    Currency() {
+	        string fileContent;
+	        string line;
+	
+	        ifstream MyReadFile("currencies.json");
+	
+	        while (getline (MyReadFile, line)) {
+	            fileContent += line;
+	        }
+	
+	        currencyData = json::parse(fileContent);
+	    }
+
+        int getCurrencyRate(string baseCurr, string convCurr) {
+            try {
+                return this -> currencyData[baseCurr][convCurr];
+            } catch (json::type_error e) {
+                cout<<e.what()<<endl;
+                return -1;
+            }
+        }
+};
 
 int main() {
-    cout<<"Hello world!";
+    Currency currencyObj;
+    
+    cout<<currencyObj.getCurrencyRate("usde","pkr");
+
     return 0;
 }
